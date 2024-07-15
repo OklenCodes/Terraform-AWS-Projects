c3fbf100c9225b0010774eeb674a2101d347169b
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-15 19:43:54 +0100

    Update outputs.tf

--- AutoScalingGroup/outputs.tf
+++ AutoScalingGroup/outputs.tf
@@ -1,11 +1,11 @@
 output "lb_endpoint" {
-  value = "http://${aws_lb.terramino.dns_name}"
+  value = "http://${aws_lb.terratutorial.dns_name}"
 }
 
 output "application_endpoint" {
-  value = "http://${aws_lb.terramino.dns_name}/index.php"
+  value = "http://${aws_lb.terratutorial.dns_name}/index.php"
 }
 
 output "asg_name" {
-  value = aws_autoscaling_group.terramino.name
+  value = aws_autoscaling_group.terratutorial.name
 }

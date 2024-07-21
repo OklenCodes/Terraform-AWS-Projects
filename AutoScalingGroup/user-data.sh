de22ed4250679ef50fb74dbad76f7990e580afaa
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-21 18:26:10 +0100

    Update user-data.sh

--- AutoScalingGroup/user-data.sh
+++ AutoScalingGroup/user-data.sh
@@ -1,15 +1,11 @@
+#!/bin/bash
 yum update -y
-yum -y remove httpd
-yum -y remove httpd-tools
-yum install -y httpd24 php72 mysql57-server php72-mysqlnd
-service httpd start
-chkconfig httpd on
-
-usermod -a -G apache ec2-user
-chown -R ec2-user:apache /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} \;
-find /var/www -type f -exec chmod 0664 {} \;
-cd /var/www/html
-curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
-curl https://raw.githubusercontent.com/oklencodes/Terraform-AWS-Tutorials/main/assets/index.php -O
+yum install httpd.x86_64 -y
+systemctl start httpd
+systemctl enable httpd
+echo '<!DOCTYPE html>' > /var/www/html/index.html
+echo '<html lang="en">' >> /var/www/html/index.html
+echo '<body style="background-color:black;">' >> /var/www/html/index.html
+echo '  <h1 style="color:red;">This is a quick web page Oklen is using to keep things simple!</h1>' >> /var/www/html/index.html
+echo '</body>' >> /var/www/html/index.html
+echo '</html>' >> /var/www/html/index.html

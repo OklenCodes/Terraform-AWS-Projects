af61aa2d5c55d237ec4f5ecf321e305506304262
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-27 12:59:08 +0100

    Update variable.tf

--- IAMPolicies/variable.tf
+++ IAMPolicies/variable.tf
@@ -1,1 +1,14 @@
+variable "aws_region" {
+  description = "storing of region from aws console"
+  default     = "us-west-1"
+}
 
+variable "s3_bucket_name" {
+  type = string
+  default = "mahira-s3-bucket"
+}
+
+variable "user_name" {
+  type = string
+  default = "mahira_user"
+}

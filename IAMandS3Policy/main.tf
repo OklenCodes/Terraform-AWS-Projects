1ee828173284ca1275dcf0ba7fe06cff79c3f8f2
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-28 18:55:33 +0100

    Update main.tf

--- IAMandS3Policy/main.tf
+++ IAMandS3Policy/main.tf
@@ -1,22 +1,23 @@
-resource "aws_s3_bucket" "log_bucket" {  #Note that S3 bucket names must be globally unique amongst all AWS customers.
-  bucket = "oklen-extremely-unique"
-    tags = {
-      Name        = "MyBucketLog"
-      Environment = "Production"
+resource "aws_s3_bucket" "log_bucket" {
+    bucket = "oklen-extremely-unique" #Note that bucket names must be globally unique amongst all aws customers
+        tags = {
+            Name            = "MyBucketLog"
+            Environment     = "Test"
     }
 }
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.log_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+
+resource "aws_s3_bucket_versioning" "versoning_example" {
+    bucket = aws_s3_bucket.log_bucket.id
+    versioning_configuration {
+        status = "Enabled"
+    }  
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.log_bucket.id
-  policy = jsonencode(
-    {
-    "Version": "2012-10-17",
+    bucket = aws_s3_bucket.log_bucket.id 
+    policy = jsonencode(
+        {
+        "Version": "2012-10-17",
     "Statement": [
         {
             "Effect": "Allow",
@@ -26,7 +27,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
             "Action": [
                 "s3:ListBucket",
                 "s3:GetObject",
-                "s3:PutObject"
+                "s3:PutObject"               
             ],
             "Resource": [
                 "arn:aws:s3:::oklen-extremely-unique",
@@ -39,6 +40,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 }
 
 resource "aws_iam_user" "user" {
-  name = "tester"
-  path = "/"
+    name = "tester"
+    path = "/"
+  
 }

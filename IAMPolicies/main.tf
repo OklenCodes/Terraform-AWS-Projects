c9b479ac382dbd5691ae39abf43653885a31cc52
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-28 13:49:36 +0100

    Update main.tf

--- IAMPolicies/main.tf
+++ IAMPolicies/main.tf
@@ -1,10 +1,15 @@
-
 resource "aws_s3_bucket" "log_bucket" {  #Note that S3 bucket names must be globally unique amongst all AWS customers.
   bucket = "oklen-extremely-unique"
     tags = {
       Name        = "MyBucketLog"
       Environment = "Production"
     }
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -33,27 +38,6 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   })
 }
 
-/*
-resource "aws_iam_user_policy" "aws_s3_bucket" {
-  name = "s3BucketPolicy"
-  user = "tester"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-*/
 resource "aws_iam_user" "user" {
   name = "tester"
   path = "/"

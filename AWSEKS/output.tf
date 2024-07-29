8147825805f82e3021575803ecc19c987ce3a204
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-29 14:59:07 +0100

    Create output.tf

---
+++ AWSEKS/output.tf
@@ -0,0 +1,24 @@
+output "cluster_id" {
+  description = "EKS cluster ID."
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane."
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider if `enable_irsa = true`."
+  value = module.eks.oidc_provider_arn
+}

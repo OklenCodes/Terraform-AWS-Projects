004a4fe51c13cb1a0e49d085b16d0d754d0666c6
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-29 15:01:02 +0100

    Create providers.tf

---
+++ AWSEKS/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.30.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.52.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+  }
+}

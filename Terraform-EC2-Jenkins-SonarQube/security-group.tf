2e1fe219908d92d1a4128aabbced0a96ea2ea82f
OklenCodes <oklen.blake@hotmail.co.uk>
2024-07-29 14:54:39 +0100

    Create security-group.tf

---
+++ Terraform-EC2-Jenkins-SonarQube/security-group.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "jenkins-sg" {
+  name = "jenkins-sg"
+  description = "jenkins security group"
+
+ingress { #HTTPS
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { #SSH
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { #Jenkins
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { #Sonarqube
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress { #HTTP
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress { #Allow everything Outbound
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo"
+  }
+}

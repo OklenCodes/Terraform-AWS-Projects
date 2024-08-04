43022d3791ddddb62f48f5488b4292bad9dd6fbd
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-04 13:35:01 +0100

    Update main.tf

--- TerraformLambda/main.tf
+++ TerraformLambda/main.tf
@@ -1,1 +1,33 @@
+provider "aws" {
+  region = var.aws_region
 
+  default_tags {
+    tags = {
+      hashicorp-learn = "lambda-api-gateway"
+    }
+  }
+
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_ownership_controls" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.lambda_bucket]
+
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}

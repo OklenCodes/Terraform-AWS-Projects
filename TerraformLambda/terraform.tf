38d3e0f23104c2c72a0a8f6b09cda237b8c01845
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-04 13:35:41 +0100

    Update terraform.tf

--- TerraformLambda/terraform.tf
+++ TerraformLambda/terraform.tf
@@ -1,1 +1,25 @@
+terraform {
 
+  cloud {
+    workspaces {
+      name = "learn-terraform-lambda-api-gateway"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
+    }
+  }
+
+  required_version = "~> 1.2"
+}

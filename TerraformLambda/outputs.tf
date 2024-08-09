3e53aa0d9d0a429e4345ebc27ab02030d11ce2fb
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-09 18:28:15 +0100

    Update outputs.tf

--- TerraformLambda/outputs.tf
+++ TerraformLambda/outputs.tf
@@ -1,7 +1,16 @@
-# Output definitions
-
 output "lambda_bucket_name" {
-  description = "Name of the S3 bucket used to store function code."
+    description = "Name of the s3 bucket used to store function code."
 
-  value = aws_s3_bucket.lambda_bucket.id
+    value = aws_s3_bucket.lambda_bucket.id
+}
+
+output "function_name" {
+    description = "Name of the Lambda Function"
+    value = aws_lambda_function.hello_world.function_name
+}
+
+output "base_url" {
+ description = "Base URL for API Gateway stage."
+
+ value = aws_apigatewayv2_stage.lambda.invoke_url
 }
